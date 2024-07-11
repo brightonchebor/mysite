357daa90cb894453b3d1d34ee8b7a33ed5d2c9bf
Chebor <cheborbrighton805@gmail.com>
2024-07-11 18:37:04 +0300

    updated

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404 
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
 from .models import Post, Comment
 from .forms import NewCommentForm
 
@@ -12,13 +12,30 @@ def home(request):
 def post_single(request, post):
 
     post = get_object_or_404(Post, slug=post, status='published')
-    comments = post.comments.flter(status=True)
+    comments = post.comments.filter(status=True)
     user_comment = None
 
     if request.method == "POST":
         comment_form = NewCommentForm(request.POST)
         if comment_form.is_valid():
             user_comment = comment_form.save(commit=False)
+            user_comment.post = post
+            user_comment.save()
+            return HttpResponseRedirect('/' + post.slug)
+    else:
+        comment_form = NewCommentForm()
+    
+    return render(
+        request, 
+        'single.html',
+        {
+            'post':post, 
+            'comments':user_comment, 
+            'comments':comments, 
+            'comment_form':comment_form
+         },
+    )
+
 
 
 
