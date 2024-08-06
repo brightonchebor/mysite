033da7dbc2d23296f1317b2296891d0b41b674ca
Chebor <cheborbrighton805@gmail.com>
2024-08-06 17:20:27 +0300

    cn

--- blog/models.py
+++ blog/models.py
@@ -51,7 +51,9 @@ class Post(models.Model):
 
 class Comment(models.Model):
     
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(Post, 
+                             on_delete=models.CASCADE, 
+                             related_name="comments")
     name = models.CharField(max_length=50)
     email = models.EmailField()
     content = models.TextField()
